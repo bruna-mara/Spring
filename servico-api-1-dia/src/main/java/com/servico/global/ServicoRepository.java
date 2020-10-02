e99de405b1430e75090fd10fdb8bfca330104324
unknown <b.maraa0805@gmail.com>
2020-10-02 10:46:55 -0300

    atividade 01-10 - API SERVICO ate POST

---
+++ servico-api-1-dia/src/main/java/com/servico/global/ServicoRepository.java
@@ -0,0 +1,13 @@
+package com.servico.global;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<ServicoModel, Long>{
+
+	List<ServicoModel> findByNome(String nome);
+
+	
+	
+}
